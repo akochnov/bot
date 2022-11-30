0abe558a12be4a3480a8ff461ba95db5d6b0736e
alexkochnov <aleksey.kochnov@gmail.com>
2022-11-30 11:42:51 +0400

    Add and configure ESLint

--- bot.js
+++ bot.js
@@ -1,43 +1,42 @@
 const dotenv = require("dotenv");
 const { Telegraf } = require("telegraf");
-const axios = require("axios");
 
-const tr = require("./translator")
+const tr = require("./translator");
 
 dotenv.config();
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-const users = process.env.TELEGRAM_USERS.split(' ');
+const users = process.env.TELEGRAM_USERS.split(" ");
 users.push(process.env.TELEGRAM_BOT_OWNER);
 
 
 bot.start((ctx) => {
     if (users.includes(ctx.update.message.from.username)) {
-        ctx.reply("Yo!")
+        ctx.reply("Yo!");
     }
     else {
-        ctx.reply("User not found")
+        ctx.reply("User not found");
     }
 });
 
 
-bot.on('text', ctx => {
+bot.on("text", ctx => {
     const original = ctx.message.text;
     console.log(original);
 
     if (users.includes(ctx.update.message.from.username)) {
 
-       tr.translate(original).then(translation => ctx.reply(translation));
+        tr.translate(original).then(translation => ctx.reply(translation));
 
     }
-})
+});
 
 
 bot.launch();
-console.log("Bot is running...")
+console.log("Bot is running...");
 
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
